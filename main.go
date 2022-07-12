8af413d658ac71c069c2b7d564e01bfcac3f17e3
sigit.darmawan <sigit.darmawan@bankraya.co.id>
2022-07-12 19:20:56 +0700

    add routes in main.go (must be errors happen)

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import "github.com/gorilla/mux"
+
 func main() {
 
+}
+
+func RegisterProductRoutes(r *mux.Router) {
+	r.HandleFunc("/api/products", controllers.GetProducts).Methods("GET")
+	r.HandleFunc("/api/products/{id}", controllers.GetProductById).Methods("GET")
+	r.HandleFunc("/api/products", controllers.CreateProduct).Methods("POST")
+	r.HandleFunc("/api/products/{id}", controllers.UpdateProduct).Methods("PUT")
+	r.HandleFunc("/api/product/{id}", controllers.DeleteProduct).Methods("DELETE")
 }
