75b5e499007efe203a69022cb7e4a0d320853452
sigit.darmawan <sigit.darmawan@bankraya.co.id>
2022-07-12 19:54:21 +0700

    add GetProductById controller and checkifProcustExists method

--- controllers/productcontroller.go
+++ controllers/productcontroller.go
@@ -5,6 +5,8 @@ import (
 	"cerud/entities"
 	"encoding/json"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func CreateProduct(w http.ResponseWriter, r *http.Request) {
@@ -12,6 +14,28 @@ func CreateProduct(w http.ResponseWriter, r *http.Request) {
 	var p entities.Product
 	json.NewDecoder(r.Body).Decode(&p)
 	database.Debe.Create(&p)
+	json.NewEncoder(w).Encode(p)
+
+}
+
+func checkIfProductExists(productId string) bool {
+	var p entities.Product
+	database.Debe.First(&p, productId)
+	if p.ID == 0 {
+		return false
+	}
+	return true
+}
+
+func GetProductById(w http.ResponseWriter, r *http.Request) {
+	pId := mux.Vars(r)["id"]
+	if !checkIfProductExists(pId) {
+		json.NewEncoder(w).Encode("Product tidak ditemukan")
+		return
+	}
+	var p entities.Product
+	database.Debe.First(&p, pId)
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(p)
 
 }
